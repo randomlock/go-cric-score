20111c95ad31aa7b6cbf58f52f104f767f435fa1
abdullah <abdullahh@lambdatest.com>
2021-12-07 02:21:51 +0530

    initial commit

--- main.go
+++ main.go
@@ -33,7 +33,63 @@ func main() {
         inning.Print()
     }
 
-    matchMap := matchController.GetMatchInfo(match.Id())
-    println(matchMap.)
 
 }
+
+// OutPut
+//
+// player - player 4 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 0 - run scored - 25 ball played - 6 4s - 1 6s - 3
+// player - player 1 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 2 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 3 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// =============================================
+// player - player 3 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 4 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 0 - run scored - 33 ball played - 10 4s - 1 6s - 3
+// player - player 1 - run scored - 8 ball played - 2 4s - 0 6s - 0
+// player - player 2 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// =============================================
+// player - player 4 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 0 - run scored - 4 ball played - 2 4s - 0 6s - 0
+// player - player 1 - run scored - 21 ball played - 4 4s - 1 6s - 2
+// player - player 2 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 3 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// =============================================
+// player - player 0 - run scored - 16 ball played - 5 4s - 0 6s - 1
+// player - player 1 - run scored - 33 ball played - 7 4s - 1 6s - 3
+// player - player 2 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 3 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// player - player 4 - run scored - 0 ball played - 0 4s - 0 6s - 0
+// =============================================
+// Run Scored - 6 - BallType - 2
+// Run Scored - 4 - BallType - 2
+// Run Scored - 6 - BallType - 2
+// Run Scored - 6 - BallType - 2
+// Run Scored - 2 - BallType - 2
+// Run Scored - 1 - BallType - 2
+// -------------------------
+// Run Scored - 2 - BallType - 2
+// Run Scored - 3 - BallType - 2
+// Run Scored - 5 - BallType - 2
+// Run Scored - 1 - BallType - 2
+// Run Scored - 3 - BallType - 2
+// Run Scored - 2 - BallType - 2
+// -------------------------
+// ******************************
+// Run Scored - 1 - BallType - 2
+// Run Scored - 6 - BallType - 2
+// Run Scored - 5 - BallType - 2
+// Run Scored - 3 - BallType - 2
+// Run Scored - 4 - BallType - 2
+// Run Scored - 6 - BallType - 2
+// -------------------------
+// Run Scored - 6 - BallType - 2
+// Run Scored - 3 - BallType - 2
+// Run Scored - 6 - BallType - 2
+// Run Scored - 1 - BallType - 2
+// Run Scored - 3 - BallType - 2
+// Run Scored - 5 - BallType - 2
+// -------------------------
+// ******************************
+
